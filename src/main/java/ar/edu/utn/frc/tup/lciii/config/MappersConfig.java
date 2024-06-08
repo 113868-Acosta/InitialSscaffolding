1a0677b0617285fd404cfa6401246b33a9f34a01
113868_Acosta_Agostina <113868@tecnicatura.frc.utn.edu.ar>
2024-06-08 14:59:19 -0300

    First Commit

---
+++ src/main/java/ar/edu/utn/frc/tup/lciii/config/MappersConfig.java
@@ -0,0 +1,39 @@
+package ar.edu.utn.frc.tup.lciii.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MappersConfig {
+
+    @Bean  //model mapper es una libreria
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+
+    /*
+    Esta anotación se utiliza para indicar inequivocamente cual es el nombre que
+    tendrá un Bean. De esta manera podemos tener mas de Bean del mismo tipo en al ApplicationContext.
+     */
+
+
+    @Qualifier("mergerMapper") // calificado, como se llama el bean
+
+    public ModelMapper mergerMapper() {
+        ModelMapper mapper =  new ModelMapper();
+        mapper.getConfiguration()
+                .setPropertyCondition(Conditions.isNotNull());
+        return mapper;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
